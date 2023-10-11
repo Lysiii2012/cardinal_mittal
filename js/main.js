7b61449ae9ddab45ecbc48aa005e2e0d77205bd4
Людмила Ивченко <95207405+Lysiii2012@users.noreply.github.com>
2023-10-11 21:10:57 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,27 @@
+let call = document.querySelectorAll('.call')
+    body = document.body
+    form = document.querySelector('.form-control')
+
+call.forEach(link => {
+    link.addEventListener('click', (e) => {
+        e.preventDefault()
+        body.classList.add('overlay')
+        form.classList.add('show')
+    }) 
+})
+
+document.querySelector('.close-pop-up').addEventListener('click', (e) => {
+    e.preventDefault()
+    body.classList.remove('overlay')
+    form.classList.remove('show')
+})
+
+const icons = document.querySelectorAll('.icon');
+icons.forEach (icon => {  
+  icon.addEventListener('click', (event) => {
+    event.preventDefault()
+    icon.classList.toggle("open");
+    document.querySelector('.header-nav').classList.toggle('open')
+  });
+});
+
